743fff1d09dbd9f69e5b2578f4312bd852ad7f26
jair quinto <giovanni.quinto02@gmail.com>
2026-04-23 15:01:22 -0500

    Migraciones aplicadas y base de datos creada

--- obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50edc74c64fe3fc83f7549c0a70539ee2d1bbbdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
