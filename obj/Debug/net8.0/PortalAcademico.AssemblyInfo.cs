2f377242228601c0aca86fd6afa5bc997f0f18d3
jair quinto <giovanni.quinto02@gmail.com>
2026-04-23 15:32:40 -0500

    Fix final seed usuarios + scope

--- obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+743fff1d09dbd9f69e5b2578f4312bd852ad7f26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec82ba063097bafabb1b8f633b0678067178e328")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
